6756bd27e2526fb556088f2daf418e782d348659
Shaonny <159715300+ShaonnyAguilar18@users.noreply.github.com>
2026-06-11 21:03:15 -0600

    Semana4

--- tiendaTech/src/main/java/com/tiendaTech/tienda/repository/CategoriaRepository.java
+++ tiendaTech/src/main/java/com/tiendaTech/tienda/repository/CategoriaRepository.java
@@ -9,5 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
     public List<Categoria> findByActivoTrue();
+     
+ 
 }
  
